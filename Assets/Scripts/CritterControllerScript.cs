a4db406ed52494fe4b0400dc8492fab2c0e74d1c
Chris C Cerami <chrisccerami@gmail.com>
2018-06-07 14:21:24 -0400

    Initial commit. Following/waiting implemented

---
+++ Assets/Scripts/CritterControllerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CritterControllerScript : MonoBehaviour, Interactable {
+	Rigidbody2D rigidBody2D;
+	public Rigidbody2D playerRigidBody;
+
+	public float maxSpeed = 1f;
+	public bool following = false;
+
+	bool facingRight = true;
+
+	void Start () {
+		rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate () {
+		float move = moveSpeed();
+		rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
+
+		if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+			Flip();
+	}
+
+	void Flip () {
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+
+	float moveSpeed() {
+		if (following) {
+			if (playerRigidBody.position.x + 1 > rigidBody2D.position.x) {
+				return 0.2f;
+			} else if (playerRigidBody.position.x - 1 < rigidBody2D.position.x) {
+				return -0.2f;
+			} else {
+				return 0;
+			}
+		} else return 0;
+	}
+
+	public void interact(KeyCode keyCode) {
+		if (keyCode == KeyCode.F) {
+			following = true;
+		} else if (keyCode == KeyCode.W) {
+			following = false;
+		}
+	}
+}
---
+++ Assets/Scripts/MainCharacterControllerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharacterControllerScript : MonoBehaviour {
+
+	Rigidbody2D rigidBody2D;
+
+	public float maxSpeed = 7f;
+	public float jumpForce = 450f;
+	public bool colliding = false;
+
+	bool facingRight = true;
+	bool grounded = false;
+	float groundRadius = 0.2f;
+	float interactRadius = 1.0f;
+
+	public GameObject interactable;
+
+	public Transform groundCheck;
+	public LayerMask whatIsGround;
+	public Transform interactCheck;
+	public LayerMask whatIsCritter;
+
+	void Start() {
+		rigidBody2D = GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		if (grounded && Input.GetKeyDown(KeyCode.Space))
+			rigidBody2D.AddForce(new Vector2(0, jumpForce));
+		interactWithObject();
+	}
+
+	void FixedUpdate() {
+		grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+		float move = Input.GetAxis("Horizontal");
+		rigidBody2D.velocity = new Vector2(move * maxSpeed, rigidBody2D.velocity.y);
+
+		if ((move > 0 && !facingRight) || (move < 0 && facingRight))
+			flip();
+		Collider2D critterCollision = Physics2D.OverlapCircle(interactCheck.position, interactRadius, whatIsCritter);
+		if (critterCollision)
+			interactable = critterCollision.gameObject;
+		else
+			interactable = null;
+	}
+
+	void flip() {
+		facingRight = !facingRight;
+		Vector3 scale = transform.localScale;
+		scale.x *= -1;
+		transform.localScale = scale;
+	}
+
+	void interactWithObject() {
+		if (interactable) {
+			CritterControllerScript controller = interactable.GetComponent(typeof(CritterControllerScript)) as CritterControllerScript;
+			if (Input.GetKeyDown(KeyCode.F))
+				controller.interact(KeyCode.F);
+			if (Input.GetKeyDown(KeyCode.W))
+				controller.interact(KeyCode.W);
+		}
+	}
+}
